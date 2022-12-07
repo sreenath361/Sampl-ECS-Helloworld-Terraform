9510e7a9ce35b01b7d4951fe99cce6fd23bcf16a
root <root@ip-10-32-12-187.ec2.internal>
2022-12-07 12:32:29 +0000

    Modified Secrets

--- versions.tf
+++ versions.tf
@@ -8,9 +8,7 @@ terraform {
 }
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIATX7VAWSQDTQG354B"
-  secret_key = "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"
-
+  profile = "default"
   default_tags {
     tags = {
       Name = "architect-demo"
